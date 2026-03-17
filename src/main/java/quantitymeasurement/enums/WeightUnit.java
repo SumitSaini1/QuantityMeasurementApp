f5becc311cc6f191ebb0e2faeef551a407bf5377
SumitSaini1 <sumitsaini21027@gmail.com>
2026-03-17 16:49:05 +0530

    [Sumit Saini] feat: Integrate Spring backend

---
+++ src/main/java/quantitymeasurement/enums/WeightUnit.java
@@ -0,0 +1,18 @@
+package quantitymeasurement.enums;
+import quantitymeasurement.util.IMeasurable;
+public enum WeightUnit implements IMeasurable {
+
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.453592);
+
+    private final double conversionFactor;
+
+    WeightUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
+}
---
+++ src/main/java/quantitymeasurement/model/QuantityModel.java
@@ -0,0 +1,10 @@
+// package quantitymeasurement.model;
+
+// import quantitymeasurement.util.IMeasurable;
+
+// public class QuantityModel {
+
+//     public double value;
+//     public IMeasurable unit;
+
+// }
