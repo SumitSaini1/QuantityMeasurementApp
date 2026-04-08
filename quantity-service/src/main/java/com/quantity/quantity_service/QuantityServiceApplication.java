7977b701a07795f28901ffa7ae87f9719fc264e9
SumitSaini1 <sumitsaini21027@gmail.com>
2026-04-08 12:00:14 +0530

    [Sumit Saini] add: fix env isuues

--- quantity-service/src/main/java/com/quantity/quantity_service/QuantityServiceApplication.java
+++ quantity-service/src/main/java/com/quantity/quantity_service/QuantityServiceApplication.java
@@ -10,6 +10,9 @@ public class QuantityServiceApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
 		SpringApplication.run(QuantityServiceApplication.class, args);
 	}
 
