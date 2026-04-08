80738983f41da8535c6f075db06160729a925639
SumitSaini1 <sumitsaini21027@gmail.com>
2026-04-08 10:59:28 +0530

    [Sumit Saini] add: database credentials

--- quantity-service/src/main/java/com/quantity/quantity_service/QuantityServiceApplication.java
+++ quantity-service/src/main/java/com/quantity/quantity_service/QuantityServiceApplication.java
@@ -3,10 +3,13 @@ package com.quantity.quantity_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class QuantityServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
 		SpringApplication.run(QuantityServiceApplication.class, args);
 	}
 
