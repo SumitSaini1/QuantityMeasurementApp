2f2ba25d72ada4aca47f6ac6e15b17b38a346982
SumitSaini1 <sumitsaini21027@gmail.com>
2026-04-08 12:05:03 +0530

    [Sumit Saini] add: fix env

--- auth-service/src/main/java/com/quantity/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/quantity/auth_service/AuthServiceApplication.java
@@ -11,9 +11,12 @@ public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
-
+		
 		System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
 		System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
