d27cbd67a63dd64d0d9461ee19641c8e9f415b36
SumitSaini1 <sumitsaini21027@gmail.com>
2026-04-11 03:37:35 +0530

    [Sumit Saini] fix: core issue

--- auth-service/src/main/java/com/quantity/auth_service/config/SwaggerConfig.java
+++ auth-service/src/main/java/com/quantity/auth_service/config/SwaggerConfig.java
@@ -2,10 +2,13 @@ package com.quantity.auth_service.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +19,9 @@ public class SwaggerConfig {
         final String securitySchemeName = "BearerAuth";
 
         return new OpenAPI()
+                
+                .servers(List.of(new Server().url("/")))
+
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName,
